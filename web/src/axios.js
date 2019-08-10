c2320b210e97a0d6db83c48a4aca8eb9bf9e5c78
AaronKwong929 <AaronKwong929@gmail.com>
2019-08-10 15:31:37 +0800

    重新封装Axios

---
+++ web/src/axios.js
@@ -0,0 +1,31 @@
+import Axios from 'axios';
+import store from './vuex/store';
+import router from './router';
+
+Axios.interceptors.request.use(
+    config => {
+        if (store.state.token) {
+            const token = store.state.token;  // 记得写成store.state.token，不然爆炸了
+            config.headers.common['Authorization'] = `Bearer ${token}`;
+        }
+        return config;
+    },
+    error => {
+        // 用不上
+        return Promise.reject(error);
+    }
+);
+
+Axios.interceptors.response.use(
+    response => {
+        return response;
+    },
+    error => {
+        if (error.response) {
+            if (error.response.status === 401) {
+                router.push('401');
+            }
+        }
+    }
+);
+export default Axios;
