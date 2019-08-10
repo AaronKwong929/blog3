4458b2c34e7507e7ce64f8c2f997ee88894ed460
AaronKwong929 <AaronKwong929@gmail.com>
2019-08-10 15:32:47 +0800

    管理员页面

---
+++ web/src/routes/Admin.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="admin">
+        {{ List }}
+        <hr />
+    </div>
+</template>
+
+<script>
+// import store from "../vuex/store.js";
+import { mapState, mapActions, mapGetters } from "vuex";
+export default {
+    data() {
+        return {};
+    },
+    methods: {},
+    computed: {
+        ...mapState({
+            List: state => {
+                return state.articleList;
+            }
+        })
+    },
+    mounted() {
+        this.$store.dispatch("GET_ARTICLES");
+    },
+    // store
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
