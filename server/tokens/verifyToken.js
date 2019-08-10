2558848009ba05522f5dd45be8834b62e5269796
AaronKwong929 <AaronKwong929@gmail.com>
2019-08-11 01:29:27 +0800

    去掉了检查token的存在

--- server/tokens/verifyToken.js
+++ server/tokens/verifyToken.js
@@ -2,12 +2,12 @@ const jwt = require('jsonwebtoken');
 const { secret } = require('../configs/jwt');
 
 module.exports = async (ctx, next) => {
-    // 检验是否存在 token
+    // 检验是否存在 token （其实已经没必要了，需要验证的发送过来一定有token）
     const authorization = ctx.request.header.authorization;
     // console.log(authorization);
-    if (!authorization) {
-        ctx.throw(401, 'Token不存在');
-    }
+    // if (!authorization) {
+    //     ctx.throw(401, 'Token不存在');
+    // }
     const token = authorization.split(' ')[1];
     // console.log(token);
     // 检验 token 是否已过期
