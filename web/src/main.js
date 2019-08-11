e3429756e55272c0ca3e5f891b61c6afdabb098c
AaronKwong929 <AaronKwong929@gmail.com>
2019-08-11 23:54:23 +0800

    优化冗余代码

--- web/src/main.js
+++ web/src/main.js
@@ -1,8 +1,6 @@
 import Vue from 'vue';
 import App from './App.vue';
 
-// import Axios from 'axios';
-
 import Axios from './axios';
 
 import './static/css/style.scss';
@@ -12,17 +10,6 @@ import router from './router';
 import store from './vuex/store';
 
 Vue.config.productionTip = false;
-
-// Axios.interceptors.response.use(
-//     response => {
-//         return response.data;
-//         // 写成response.data 前端即可直接res而无需res.data
-//     },
-//     err => {
-//         return Promise.resolve(err.response);
-//         // throw下 注意是resolve， reject会控制台输出错误
-//     }
-// );
 
 Vue.prototype.$axios = Axios;
 
