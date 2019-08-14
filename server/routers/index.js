5502b2664fe75327e2ea2d242cd5c14842419f84
AaronKwong929 <AaronKwong929@gmail.com>
2019-08-14 22:02:51 +0800

    加公用路由

--- server/routers/index.js
+++ server/routers/index.js
@@ -1,6 +1,9 @@
 const Router = require('koa-router')();
 const adminRouter = require('./admin');
+const commonRouter = require('./common');
 
 Router.use('/admin', adminRouter.routes(), adminRouter.allowedMethods());
+
+Router.use('/common', commonRouter.routes(), commonRouter.allowedMethods());
 
 module.exports = Router;
