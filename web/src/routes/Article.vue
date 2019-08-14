d65a7503676afa583ec5be6aa37ecf0ab8179fd4
AaronKwong929 <AaronKwong929@gmail.com>
2019-08-14 22:12:42 +0800

    详情页没样式

---
+++ web/src/routes/Article.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="Article">
+        {{ details.title }}
+        {{ details.type }}
+        {{ details.tag }}
+        {{ details.updatedAt }}
+        {{ details.content }}
+    </div>
+</template>
+<script>
+// import { mapActions } from "vuex";
+export default {
+    data() {
+        return {
+            details: ""
+        };
+    },
+    methods: {
+        // ...mapActions({
+        //     findArticle: "FIND_ARTICLE"
+        // }),
+        getDetails() {
+            this.details = this.$store.state.articleList.find(item => {
+                return item._id === this.$route.params.id;
+            });
+
+            // this.details = this.findArticle(this.$route.params.id);
+
+            // this.details = this.$store.dispatch('FIND_ARTICLE', this.$route.params.id)
+        }
+    },
+    mounted() {
+        this.getDetails();
+    }
+};
+</script>
+<style lang="scss" scoped>
+</style>
