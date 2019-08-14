6e25d58596c9c2f80fa29e94c6c27f8dc70f7481
AaronKwong929 <AaronKwong929@gmail.com>
2019-08-14 22:04:11 +0800

    全局指令和样式引入

--- web/src/main.js
+++ web/src/main.js
@@ -6,12 +6,23 @@ import Axios from './axios';
 import './static/css/style.scss';
 import './static/icon/iconfont.css';
 
+import './static/css/atom-one-dark.css';
+
 import router from './router';
 import store from './vuex/store';
+
+import hljs from 'highlight.js';
 
 Vue.config.productionTip = false;
 
 Vue.prototype.$axios = Axios;
+
+Vue.directive('highlight', function(el) {
+    let blocks = el.querySelectorAll('pre code');
+    blocks.forEach(block => {
+        hljs.highlightBlock(block);
+    });
+});
 
 new Vue({
     render: h => h(App),
