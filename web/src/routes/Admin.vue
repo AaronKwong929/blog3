c35e684d91a53efe045b973790d3594c51e84039
AaronKwong929 <AaronKwong929@gmail.com>
2019-08-14 22:09:33 +0800

    管理页未写样式

--- web/src/routes/Admin.vue
+++ web/src/routes/Admin.vue
@@ -1,10 +1,21 @@
 <template>
     <div id="admin">
-        {{ List }}
-        <hr />
         <button @click="logout" class="btn btn-small">注销</button>
-        <button @click="newArticle">新建</button>
-        <router-view></router-view>
+        <button @click="newArticle" class="btn btn-small">新建</button>
+        <div class="list">
+            <div
+                :to="'/draft/' + item._id"
+                v-for="(item, index) in List"
+                :key="index"
+            >
+                <span class="title">{{ item.title }}</span>
+                <span class="type">{{ item.type }}</span>
+                <span class="tag">{{ item.tag }}</span>
+                <span class="time">{{ item.updatedAt }}</span>
+                <router-link :to="'/draft/' + item._id">+</router-link>
+            </div>
+            <div></div>
+        </div>
     </div>
 </template>
 
@@ -16,19 +27,25 @@ export default {
     },
     methods: {
         ...mapMutations({
-            logout: 'LOG_OUT'
+            logout: "LOG_OUT"
         }),
         ...mapActions({
-            getArticles: 'GET_ARTICLES'
+            getArticles: "GET_ARTICLES"
         }),
         newArticle() {
-            this.$axios.post('/api/admin/draft').then(
-                res => {
-                    console.log(res.data.article);
-                    this.$store.state.articleDetails = res.data.article;
-                    this.$router.push(`/draft/${this.$store.state.articleDetails._id}`);
-                }
-            )
+            this.$axios.post("/api/admin/draft").then(res => {
+                // console.log(res.data.article);
+                // 刷新state.articleList
+                this.getArticles();
+                // this.$store.state.articleDetails = res.data.article;
+                setTimeout(() => {
+                    this.$router.push(`/draft/${res.data.article._id}`);
+                }, 300);
+
+                // this.$router.push(
+                //     `/draft/${res.data.article._id}`
+                // );
+            });
         }
     },
     computed: {
