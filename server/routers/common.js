4b4ca6b6792ae03cb5f6c04713ec96b3366c771e
AaronKwong929 <AaronKwong929@gmail.com>
2019-08-17 01:24:40 +0800

    add sort filter

--- server/routers/common.js
+++ server/routers/common.js
@@ -5,7 +5,18 @@ const Article = require('../models/Articles');
 let commonRouter = new Router();
 
 commonRouter.get('/articles', async ctx => {
-    const articleList = await Article.find({});
+    let articleList = await Article.find({});
+    articleList = articleList.filter(item => {
+        return item.published === true;
+    }).sort((a, b) => {
+        let aTimeString = a.updatedAt;
+        let bTimeString = b.updatedAt;
+        aTimeString = aTimeString.replace(/-/g, '/');
+        bTimeString = bTimeString.replace(/-/g, '/');
+        let aTime = new Date(aTimeString).getTime();
+        let bTime = new Date(bTimeString).getTime();
+        return bTime - aTime
+    });
     ctx.response.body = {
         articleList
     }
