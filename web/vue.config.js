c36da6a43f90394ee45b8c0f79987699a76d6830
AaronKwong929 <AaronKwong929@gmail.com>
2019-08-25 14:33:17 +0800

    config更新

--- web/vue.config.js
+++ web/vue.config.js
@@ -7,8 +7,9 @@ module.exports = {
         host: 'localhost',
         https: false,
         open: true,  // 自动启动浏览器
+        // 打包和上线版本需要注释proxy
         proxy: {
-            '/api': {
+            '/api/*': {
                 target: 'http://127.0.0.1:3000', //设置调用的接口域名和端口
                 changeOrigin: true, //是否跨域
                 ws: true,
