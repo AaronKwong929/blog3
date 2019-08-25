dd770b69c60325ea3d091139ef802d1dbd1f14c2
AaronKwong929 <AaronKwong929@gmail.com>
2019-08-26 01:31:28 +0800

    actions update

--- web/src/vuex/actions.js
+++ web/src/vuex/actions.js
@@ -1,7 +1,11 @@
 import Axios from '../axios';
 
+// 上线地址: http://106.53.89.236:3000
+// 打包地址: http://127.0.0.1:3000 vue.config.js 注释 devServer.proxy
+// 开发地址: /api vue.config.js 取消 devServer.proxy 注释
+
 const actions = {
-    ADMIN_GET_ARTICLES({ state }) {
+    async ADMIN_GET_ARTICLES({ state }) {
         Axios.get('/api/admin').then(res => {
             state.adminArticleList = res.data.articleList;
         });
@@ -13,16 +17,16 @@ const actions = {
             state.adminArticleList = res.data.articleList;
         });
     },
-    COMMON_GET_ARTICLES({ state }) {
+    async COMMON_GET_ARTICLES({ state }) {
         Axios.get('/api/common/articles').then(res => {
             state.articleList = res.data.articleList;
         });
+    },
+    FIND_ARTICLE: ({ state }, id) => {
+        state.articleDetails = state.articleList.find(item => {
+            return item._id === id;
+        });
     }
-    // FIND_ARTICLE: (state, id) => {
-    //     return state.articleList.find(item => {
-    //         return item._id === id
-    //     });
-    // }
 };
 
 export default actions;
