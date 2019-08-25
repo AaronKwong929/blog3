5bd3e5d4ded41f61250ef6ad7739c019e389238d
AaronKwong929 <AaronKwong929@gmail.com>
2019-08-26 00:44:29 +0800

    详情页优化

--- web/src/routes/Article.vue
+++ web/src/routes/Article.vue
@@ -1,14 +1,14 @@
 <template>
     <div id="Article">
         <div class="article-title">
-            {{ details.title }}
+            {{ Details.title }}
         </div>
         <div class="article-time article-info">
-            <i class="iconfont icon-riqi"></i>{{ details.updatedAt }}
+            <i class="iconfont icon-riqi"></i>{{ Details.updatedAt }}
         </div>
         <div class="article-other article-info">
-            <i class="iconfont icon-yingyongAPP"></i>{{ details.type }}
-            <i class="iconfont icon-biaoqian"></i>{{ details.tag }}
+            <i class="iconfont icon-yingyongAPP"></i>{{ Details.type }}
+            <i class="iconfont icon-biaoqian"></i>{{ Details.tag }}
         </div>
         <div
             class="article-content"
@@ -19,6 +19,7 @@
 </template>
 <script>
 import marked from "marked";
+import { mapActions, mapState } from "vuex";
 export default {
     data() {
         return {
@@ -26,20 +27,33 @@ export default {
         };
     },
     methods: {
-        getDetails() {
-            this.details = this.$store.state.articleList.find(item => {
-                return item._id === this.$route.params.id;
-            });
-        }
+        ...mapActions({
+            getArticles: "COMMON_GET_ARTICLES",
+            getDetailss: "FIND_ARTICLE"
+        })
     },
     computed: {
         compiledMarkdown: function() {
-            return marked(this.details.content);
-        }
+            return marked(this.Details.content);
+        },
+        ...mapState({
+            Details: state => {
+                return state.articleDetails;
+            }
+        })
     },
+    // 不写 beforeMount 在获取了 list 之后再打开文章详情会报错，写了不会，但在情页刷新还会，顺序问题，暂未解决
     beforeMount() {
-        this.getDetails();
-    },
+        if (this.$store.state.articleList.length === 0) {
+            this.getArticles();
+            // 不加一秒会显示不出来 未知原因
+            setTimeout(() => {
+                this.getDetailss(this.$route.params.id);
+            }, 1000);
+        } else {
+            this.getDetailss(this.$route.params.id);
+        }
+    }
 };
 </script>
 <style lang="scss" scoped>
