2f4c7168bda45a598426cbc3f8bd758b3aea8297
AaronKwong929 <AaronKwong929@gmail.com>
2019-08-26 01:30:40 +0800

    sidebar update

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,57 +1,30 @@
 <template>
     <div id="app">
-        <div class="sidebar">
-            <Sidebar></Sidebar>
-        </div>
+        <Sidebar></Sidebar>
         <div class="router-views">
             <router-view></router-view>
-        </div>
-        <div class="subsidebar">
-            <SubSidebar></SubSidebar>
         </div>
     </div>
 </template>
 
 <script>
 import Sidebar from "./components/Sidebar";
-import SubSidebar from './components/SubSidebar'
 export default {
     components: {
-        Sidebar,
-        SubSidebar
+        Sidebar
     },
     created() {
-        if (localStorage.getItem('token')) {
-            let token = localStorage.getItem('token');
+        if (localStorage.getItem("token")) {
+            let token = localStorage.getItem("token");
             this.$store.state.token = token;
         }
-    },
+    }
 };
 </script>
 
 <style lang="scss" scoped>
-.sidebar {
-    width: 15%;
-    min-width: 10%;
-    position: fixed;
-    left: 0;
-    top: 0;
-    bottom: 0;
-    box-shadow: .6rem 0 .6rem -.6rem #aaa;
-    background-color: #ddd;
-}
 .router-views {
     margin-left: 15%;
     margin-right: 5%;
-}
-.subsidebar {
-    width: 5%;
-    height: 100%;
-    position: fixed;
-    top: 0;
-    bottom: 0;
-    right: 0;
-    background-color: #ddd;
-    box-shadow: -.6rem 0 .6rem -.6rem #aaa;
 }
 </style>
--- web/src/components/Sidebar.vue
+++ web/src/components/Sidebar.vue
@@ -1,51 +1,103 @@
 <template>
     <div id="sidebar">
-        <div class="avatar">
-            <img src="../static/img/avatar.jpg" />
+        <div class="left-bar">
+            <div class="avatar">
+                <img src="../static/img/avatar.jpg" />
+            </div>
+            <div class="routers">
+                <router-link to="/home" tag="div" class="bar-item" title="首页"
+                    ><i class="iconfont icon-cangku"></i
+                ></router-link>
+
+                <router-link
+                    to="/archive"
+                    tag="div"
+                    class="bar-item"
+                    title="归档"
+                    ><i class="iconfont icon-liebiao1"></i
+                ></router-link>
+
+                <router-link to="/type" tag="div" class="bar-item" title="分类"
+                    ><i class="iconfont icon-yingyongAPP"></i
+                ></router-link>
+
+                <router-link to="/tag" tag="div" class="bar-item" title="标签"
+                    ><i class="iconfont icon-biaoqian"></i
+                ></router-link>
+
+                <router-link to="/about" tag="div" class="bar-item" title="关于"
+                    ><i class="iconfont icon-guanyu"></i
+                ></router-link>
+
+                <router-link
+                    to="/login"
+                    tag="div"
+                    class="bar-item"
+                    title="登陆"
+                    v-if="!this.$store.state.token"
+                    ><i class="iconfont icon-login"></i
+                ></router-link>
+
+                <router-link
+                    to="/admin"
+                    tag="div"
+                    class="bar-item"
+                    title="管理"
+                    v-else
+                    ><i class="iconfont icon-gerentouxiang"></i
+                ></router-link>
+            </div>
         </div>
-        <div class="routers">
-            <router-link to="/home" tag="div" class="bar-item" title="首页"
-                ><i class="iconfont icon-cangku"></i
-            ></router-link>
+        <div class="right-bar">
+            <button class="bar-item" @click="back" title="返回">
+                <i class="iconfont icon-chexiaofanhuichehuishangyibu"></i>
+            </button>
 
-            <router-link to="/archive" tag="div" class="bar-item" title="归档"
-                ><i class="iconfont icon-liebiao1"></i
-            ></router-link>
-
-            <router-link to="/type" tag="div" class="bar-item" title="分类"
-                ><i class="iconfont icon-yingyongAPP"></i
-            ></router-link>
-
-            <router-link to="/tag" tag="div" class="bar-item" title="标签"
-                ><i class="iconfont icon-biaoqian"></i
-            ></router-link>
-
-            <router-link to="/about" tag="div" class="bar-item" title="关于"
-                ><i class="iconfont icon-guanyu"></i
-            ></router-link>
-
+            <button class="bar-item" @click="top" title="回到顶部">
+                <i class="iconfont icon-zhiding"></i>
+            </button>
+            
             <router-link
-                to="/login"
+                to="/update"
                 tag="div"
                 class="bar-item"
-                title="登陆"
-                v-if="!this.$store.state.token"
-                ><i class="iconfont icon-login"></i
-            ></router-link>
-
-            <router-link
-                to="/admin"
-                tag="div"
-                class="bar-item"
-                title="管理"
-                v-else
-                ><i class="iconfont icon-gerentouxiang"></i
+                title="开发日志"
+                ><i class="iconfont icon-custom-update"></i
             ></router-link>
         </div>
     </div>
 </template>
 
+<script>
+export default {
+    methods: {
+        back() {
+            this.$router.go(-1);
+        },
+        top() {
+            document.documentElement.scrollTop = 0;
+            document.body.scrollTop = 0;
+        },
+        toUpdate() {
+            this.$router.push("/update");
+        }
+    }
+};
+</script>
+
 <style lang="scss" scoped>
+/* 左边的bar */
+.left-bar {
+    width: 15%;
+    min-width: 10%;
+    position: fixed;
+    left: 0;
+    top: 0;
+    bottom: 0;
+    box-shadow: 0.6rem 0 0.6rem -0.6rem #aaa;
+    background-color: #ddd;
+}
+
 .router-link-active {
     background-color: rgba(96, 126, 121, 0.4);
     box-shadow: 0 0 1rem #aaa;
@@ -72,6 +124,8 @@
     display: flex;
     justify-content: center;
     align-items: center;
+    border: none;
+    outline: none;
 }
 
 .bar-item:hover,
@@ -82,7 +136,28 @@
     transition: all 0.5s;
 }
 
-.bar-item:last-child {
+.left-bar .bar-item:last-child {
     margin-top: 4rem;
 }
+
+/* 右边的bar */
+.right-bar {
+    width: 5%;
+    height: 100%;
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    background-color: #ddd;
+    box-shadow: -0.6rem 0 0.6rem -0.6rem #aaa;
+}
+
+.right-bar .bar-item:first-child {
+    margin-top: 600%;
+}
+
+.right-bar .bar-item:nth-child(n + 2) {
+    margin-top: 80%;
+}
+
 </style>
