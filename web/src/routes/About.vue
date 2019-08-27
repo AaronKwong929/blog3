f390650f8967e4b86592578ca863b2b1bd570971
AaronKwong929 <AaronKwong929@gmail.com>
2019-08-27 22:50:19 +0800

    日志，关于页更新

--- web/src/routes/About.vue
+++ web/src/routes/About.vue
@@ -1,25 +1,137 @@
 <template>
-    <div id="about" class="router-view-general">
-        <i class="iconfont icon-guanyu"></i>
-        <div class="router-title">
-            关于
+    <div id="about">
+        <div class="about-me">
+            <div class="about-title">关于我</div>
+            <div class="left">
+                <ul>
+                    <li>现居 广东 广州</li>
+                    <li>(截至 2019/8)半年前端编程经验</li>
+                    <li>大三下学期开始进行前端学习</li>
+                    <li>目前对 html，css，js 有不够深的了解</li>
+                    <li>使用过 vue 和 koa 做过小项目</li>
+                    <li>副业 python 爬虫和 数据分析</li>
+                    <li>(即将)毕业于广州大学</li>
+                    <li>计算机科学与网络工程学院 软件工程专业</li>
+                </ul>
+            </div>
+            <div class="right">
+                <a
+                    href="https://github.com/AaronKwong929"
+                    target="_blank"
+                    title="Aaron's GitHub"
+                    ><i class="iconfont icon-icon-test5"></i
+                ></a>
+                <a
+                    href="http://106.53.89.236:3000"
+                    target="_blank"
+                    title="个人主页"
+                    ><i class="iconfont icon-gerentouxiang"></i
+                ></a>
+            </div>
         </div>
-        关于我 <br/>
-        个人介绍，时间线，<br/><br/>
-        关于本博客<br/>
-        开发原因，技术栈，开发版本号，上线版本号，期待未来版本实现的内容<br/>
-        <div class="more">
-            <a
-                href="https://github.com/AaronKwong929"
-                target="_blank"
-                title="Aaron's GitHub"
-                ><i class="iconfont icon-icon-test5"></i
-            ></a>
-            <div class="additional">之后再补充</div>
+        <div class="about-blog">
+            <div class="about-title">关于本博客</div>
+            <ul>
+                <li class="li-title">开发原因</li>
+                <li class="li-content">
+                    <div class="line-through">
+                        没有自己搭建的个人博客的前端仔都是咸鱼
+                    </div>
+                    vue koa前后端分离开发尝试<br />
+                    毕业设计?
+                </li>
+                <li class="li-title">版本号</li>
+                <li class="li-content">开发版本3.2 - 上线版本1.2</li>
+                <li class="li-title">技术栈</li>
+                <li class="li-content">
+                    <ul>
+                        <li>前端：Vue.JS 全家桶</li>
+                        <li>后端：Koa.JS</li>
+                    </ul>
+                </li>
+                <li class="li-title">未来期待实现的内容</li>
+                <li class="li-content">
+                    <ul>
+                        <li>
+                            1. 移动端支持
+                        </li>
+                        <li>
+                            2.
+                        </li>
+                        <li>
+                            3.
+                        </li>
+                    </ul>
+                </li>
+            </ul>
         </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
+.about-me,
+.about-blog {
+    vertical-align: top;
+    box-shadow: 0 0 1rem 0.2rem #aaa;
+    margin: 2rem;
+    border-radius: 1rem;
+    padding: 2rem;
+}
 
+.about-title {
+    font-size: 1.5rem;
+    font-weight: 600;
+    box-shadow: 0 0.3rem 0.3rem -0.2rem #aaa;
+    text-align: center;
+    padding-bottom: 1rem;
+    margin-bottom: 1rem;
+}
+
+.about-me ul li {
+    line-height: 1.5rem;
+    font-size: 1.1rem;
+    font-weight: 300;
+    margin-bottom: 0.5rem;
+}
+
+.iconfont {
+    font-size: 2.5rem;
+    width: 4rem;
+    height: 4rem;
+}
+
+.iconfont:hover,
+.iconfont:active {
+    background-color: rgba(96, 126, 121, 0.4);
+    box-shadow: 0 0 1rem #aaa;
+    border-radius: 50%;
+    transition: all 0.5s;
+}
+
+.about-me a {
+    margin: 1rem;
+}
+
+.li-title {
+    font-size: 1.3rem;
+    font-weight: 600;
+}
+
+.li-content {
+    padding-left: 1rem;
+    font-size: 1.1rem;
+    font-weight: 300;
+    margin-bottom: 1rem;
+}
+
+.left,
+.right {
+    display: inline-block;
+    width: 50%;
+    vertical-align: top;
+}
+
+.line-through {
+    text-decoration: line-through;
+}
 </style>
--- web/src/routes/UpdateLog.vue
+++ web/src/routes/UpdateLog.vue
@@ -4,27 +4,47 @@
         <div class="router-title">
             开发日志
         </div>
-        <div class="log-details">
-            <ul class="log-items">
-                <li>
-                    <div class="ver">2019-8-26 开发版本3.1 上线版本1.1</div>
-                    <div>
-                        新增内容：<br />
-                        <ul class="new">
-                            <li>登陆持久化</li>
-                            <li>开发日志页面</li>
-                        </ul>
+        <ul class="log-items">
+            <li>
+                <div class="ver">2019-8-28 开发版本3.2 上线版本1.2</div>
+                <div class="new">
+                    <div class="new-item">边栏移动端支持</div>
+                    <div class="new-item">首页和关于页更新</div>
+                    <div class="new-item">
+                        时间格式化更新（Linux和Win下格式保持一致）
                     </div>
-                </li>
-                <li>2019-8-23 开发版本3.0 上线版本1.0</li>
-            </ul>
-        </div>
+                </div>
+            </li>
+            <li>
+                <div class="ver">2019-8-26 开发版本3.1 上线版本1.1</div>
+                <div class="new">
+                    <div class="new-item">登陆持久化</div>
+                    <div class="new-item">开发日志页面</div>
+                </div>
+            </li>
+            <li>
+                <div class="ver">2019-8-23 开发版本3.0 上线版本1.0</div>
+            </li>
+        </ul>
     </div>
 </template>
 
-<script>
-export default {};
-</script>
-
 <style lang="scss" scoped>
+.log-items > li {
+    margin: 2rem auto;
+}
+
+.ver {
+    font-size: 1.4rem;
+    font-weight: 400;
+    padding-bottom: .3rem;
+}
+
+.new {
+    line-height: 1.5rem;
+}
+
+.new-item {
+    font-size: .9rem;
+}
 </style>
