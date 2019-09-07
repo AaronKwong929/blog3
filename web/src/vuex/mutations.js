9a2cd1ff44e5599d63098dc0749be9da52698d49
AaronKwong929 <AaronKwong929@gmail.com>
2019-09-07 23:55:31 +0800

    搜索

--- web/src/vuex/mutations.js
+++ web/src/vuex/mutations.js
@@ -4,6 +4,15 @@ const mutations = {
         state.token = '';
         localStorage.removeItem('token');
         router.push('login');
+    },
+    SEARCH_ARTICLES(state) {
+        state.searchResults.titleResults = state.articleList.filter(item => {
+            return item.title.includes(state.searchTerm);
+        });
+        state.searchResults.contentResults = state.articleList.filter(item => {
+            return item.content.includes(state.searchTerm);
+        });
+        router.push('/search');
     }
 };
 
