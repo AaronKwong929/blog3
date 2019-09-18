8590724eeff6d1675f605fa6496f53c74b710e32
AaronKwong929 <AaronKwong929@gmail.com>
2019-09-18 23:58:45 +0800

    路由懒加载

--- web/src/router.js
+++ web/src/router.js
@@ -3,38 +3,24 @@ import VueRouter from 'vue-router';
 
 import store from './vuex/store';
 
-import Home from './routes/Home.vue';
-import Archive from './routes/Archive.vue';
-import Type from './routes/Type.vue';
-import Tag from './routes/Tag.vue';
-import About from './routes/About.vue';
-import Article from './routes/Article.vue';
-import Login from './routes/Login.vue';
-import Admin from './routes/Admin.vue';
-import Draft from './routes/Draft.vue';
-import NotFound from './routes/404.vue';
-import Unauthorized from './routes/401.vue';
-import UpdateLog from './routes/UpdateLog.vue';
-import SearchResult from './routes/SearchResult.vue'
-
 Vue.use(VueRouter);
 
 const routes = [
     { path: '/', redirect: 'home' },
-    { path: '/home', component: Home },
-    { path: '/archive', component: Archive },
-    { path: '/type', component: Type },
-    { path: '/tag', component: Tag },
-    { path: '/about', component: About },
-    { path: '/article/:id', component: Article },
-    { path: '/login', component: Login },
-    { path: '/admin', component: Admin, meta: { requireAuth: true } },
-    { path: '/draft/:id', component: Draft, meta: { requireAuth: true } },
+    { path: '/home', component: () => import('./routes/Home.vue') },
+    { path: '/archive', component: () => import('./routes/Archive.vue') },
+    { path: '/type', component: () => import('./routes/Type.vue') },
+    { path: '/tag', component: () => import('./routes/Tag.vue') },
+    { path: '/about', component: () => import('./routes/About.vue') },
+    { path: '/article/:id', component: () => import('./routes/Article.vue') },
+    { path: '/login', component: () => import('./routes/Login.vue') },
+    { path: '/admin', component: () => import('./routes/Admin.vue'), meta: { requireAuth: true } },
+    { path: '/draft/:id', component: () => import('./routes/Draft.vue'), meta: { requireAuth: true } },
     { path: '*', redirect: '404' },
-    { path: '/404', component: NotFound },
-    { path: '/401', component: Unauthorized },
-    { path: '/update', component: UpdateLog },
-    { path: '/search', component: SearchResult }
+    { path: '/404', component: () => import('./routes/404.vue') },
+    { path: '/401', component: () => import('./routes/401.vue') },
+    { path: '/update', component: () => import('./routes/UpdateLog.vue') },
+    { path: '/search', component: () => import('./routes/SearchResult.vue') }
 ];
 
 const router = new VueRouter({
