d059f9f6f7faca9398b40f4492e67f3390b1ab84
AaronKwong929 <AaronKwong929@gmail.com>
2019-09-19 22:29:10 +0800

    改时长

--- server/tokens/addToken.js
+++ server/tokens/addToken.js
@@ -4,6 +4,6 @@ const { secret } = require('../configs/jwt');
 module.exports = adminInfo => {
     const token = jwt.sign({
         name: adminInfo.name
-    }, secret, { expiresIn: '3h' });
+    }, secret, { expiresIn: '1d' });
     return token;
 };
