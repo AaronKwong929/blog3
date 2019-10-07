ea0082803aa140e9b5796d81011b42bd18ea120c
AaronKwong929 <AaronKwong929@gmail.com>
2019-10-07 12:09:39 +0800

    轮播组件

--- web/src/routes/Home.vue
+++ web/src/routes/Home.vue
@@ -1,20 +1,14 @@
 <template>
     <div id="home">
-        <header class="home-front">
-            <div class="home-title">Aaron's Blog</div>
-        </header>
+        <Carousal></Carousal>
     </div>
 </template>
 
-<style lang="scss" scoped>
-.home-front {
-    text-align: center;
-    padding: 3rem 0 1rem 0;
-    box-shadow: 0 0.6rem 0.6rem -0.6rem #aaa;
-    color: rgb(96, 126, 121);
-}
-
-.home-title {
-    font-size: 3rem;
-}
-</style>
+<script>
+const Carousal = () => import("../components/Carousal");
+export default {
+    components: {
+        Carousal
+    }
+};
+</script>
