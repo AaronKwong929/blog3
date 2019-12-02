7f2453d4209b6e32c8dd3cf3213bf6b4a236d89b
AaronKwong929 <AaronKwong929@gmail.com>
2019-12-02 23:21:17 +0800

    首页轮播图

--- web/src/routes/Home.vue
+++ web/src/routes/Home.vue
@@ -1,72 +1,107 @@
 <template>
     <div id="home">
-        <header>
-            <Carousal></Carousal>
-            <div class="recent">
-                <span class="recent-title">最近动态</span>
-                <div class="recent-content">
-                    {{ Recent.content }}
-                </div>
-                <div class="recent-time">
-                    {{ Recent.updatedAt }}
-                </div>
-            </div>
-        </header>
-        <div class="recent-wrapper">
-            <div class="recent-articles">
-                <span class="recent-title">最新文章</span>
-                <router-link
-                    v-for="(item, index) in List"
-                    :key="index"
-                    :to="'/article/' + item._id"
-                    tag="div"
-                    class="list-item"
+        <template>
+            <el-carousel
+                :interval="5000"
+                arrow="always"
+                indicator-position="outside"
+            >
+                <el-carousel-item
+                    v-for="item in carousalItem"
+                    :key="item.index"
                 >
-                    <div class="article-col">
-                        <span class="title item-left">{{ item.title }}</span>
-                        <span class="time item-right">{{
-                            item.updatedAt
-                        }}</span>
-                    </div>
-                    <div class="article-col">
-                        <span class="item-left">
-                            <i class="iconfont icon-yingyongAPP"></i
-                            >{{ item.type }}
-                        </span>
-                        <span class="item-right">
-                            <i class="iconfont icon-biaoqian"></i>{{ item.tag }}
-                        </span>
-                    </div>
-                </router-link>
-            </div>
-            <div class="recent-works">
-                <span class="recent-title">其他项目</span>
-                <a href="abc.aaronkwong.xyz" target="_blank">还没上线的项目2</a>
-            </div>
-        </div>
+                    <h2>
+                        {{ item.title
+                        }}<span v-if="item.subTitle">
+                            - {{ item.subTitle }}</span
+                        >
+                    </h2>
+                </el-carousel-item>
+            </el-carousel>
+        </template>
+        <el-collapse
+            v-model="activeName"
+            accordion
+            style="width: 80%; margin: 0 auto;"
+        >
+            <el-collapse-item title="最近状态" name="1">
+                <div>
+                    {{ this.$store.state.recent.content }}
+                </div>
+                <div>
+                    {{ this.$store.state.recent.updatedAt }}
+                </div>
+            </el-collapse-item>
+            <el-collapse-item title="最新文章" name="2">
+                <div>
+                    <router-link
+                        v-for="(item, index) in List"
+                        :key="index"
+                        :to="'/article/' + item._id"
+                        tag="div"
+                        class="list-item"
+                    >
+                        <div class="article-col">
+                            <span class="title item-left">{{
+                                item.title
+                            }}</span>
+                        </div>
+                        <div class="article-col">
+                            <span class="item-left">
+                                <i class="iconfont icon-yingyongAPP"></i
+                                >{{ item.type }}
+                            </span>
+                            <span class="item-right">
+                                <i class="iconfont icon-biaoqian"></i
+                                >{{ item.tag }}
+                            </span>
+                        </div>
+                    </router-link>
+                </div>
+            </el-collapse-item>
+        </el-collapse>
     </div>
 </template>
 
 <script>
-const Carousal = () => import("../components/Carousal");
-import { mapState, mapActions } from "vuex";
+import { mapState, mapActions } from 'vuex';
 export default {
-    components: {
-        Carousal
+    data() {
+        return {
+            activeName: '1',
+            carousalItem: [
+                {
+                    index: `c1`,
+                    title: `Welcome To`
+                },
+                {
+                    index: `c2`,
+                    title: `Aaron's Blog.`
+                },
+                {
+                    index: `c3`,
+                    title: `Make Yourselves At Home.`
+                },
+                {
+                    index: `c4`,
+                    title: `No Commercial Available.`
+                }
+            ]
+        };
     },
     methods: {
         ...mapActions({
-            getArticles: "COMMON_GET_ARTICLES",
-            getRecent: "GET_RECENT"
+            getArticles: 'COMMON_GET_ARTICLES',
+            getRecent: 'GET_RECENT'
         })
     },
     computed: {
         ...mapState({
             List: state => {
-                return state.articleList.slice(0, 3);
+                return state.articleList.slice(0, 5);
             },
             Recent: state => {
-                return state.recent || "";
+                return state.recent || '';
             }
         })
     },
@@ -74,7 +109,7 @@ export default {
         if (this.$store.state.articleList.length === 0) {
             this.getArticles();
         }
-        if (this.$store.state.recent === "") {
+        if (this.$store.state.recent === '') {
             this.getRecent();
         }
     }
@@ -82,55 +117,21 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-header {
-    padding-top: 1rem;
-    display: flex;
-    height: 330px;
+/deep/ .el-carousel__item:nth-child(2n) {
+    background-color: #a29bfe;
 }
-.recent {
-    width: 30vw;
-    box-shadow: 0 0 0.8rem 0 #aaa;
-    padding: 1rem;
-    border-radius: 1rem;
-    margin-bottom: 2rem;
+/deep/ .el-carousel__item:nth-child(2n + 1) {
+    background-color: #74b9ff;
 }
-.recent-title {
-    font-size: 2rem;
-    border-bottom: 1px solid rgba(96, 126, 121, 0.4);
-    align-self: flex-start;
-}
-.recent-content {
-    font-weight: 300;
-    font-size: 1.5rem;
-}
-.recent-time {
-    font-size: 0.8rem;
-    margin: 5rem 1rem 0 18vw;
-    flex-wrap: wrap;
-}
-.recent-articles {
-    box-shadow: 0 0 0.8rem 0 #aaa;
-    padding: 1rem;
-    margin: 1rem;
-    border-radius: 1rem;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    flex: 2;
-}
-.list-item {
-    cursor: pointer;
-    width: 100%;
-    padding: 0.5rem;
-    margin-top: 0.5rem;
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-}
-.list-item:hover {
-    box-shadow: 0 0 0.8rem 0 #aaa;
-    border-radius: 1rem;
-    transition: all 1s;
+h2 {
+    text-align: center;
+    font: {
+        weight: 300;
+        size: 3rem;
+    }
+    line-height: 300px;
+    color: #475669;
+    opacity: 0.75;
 }
 .item-left {
     flex-grow: 1;
@@ -141,17 +142,5 @@ header {
 }
 .article-col {
     display: flex;
-}
-.recent-wrapper {
-    display: flex;
-}
-.recent-works {
-    flex: 1;
-    box-shadow: 0 0 0.8rem 0 #aaa;
-    padding: 1rem;
-    margin: 1rem;
-    border-radius: 1rem;
-    display: flex;
-    flex-direction: column;
 }
 </style>
