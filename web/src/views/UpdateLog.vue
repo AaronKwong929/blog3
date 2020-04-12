c06026ae659428b835da1238907a27bcc518ebb8
AaronKwong929 <AaronKwong929@gmail.com>
2020-04-13 00:00:40 +0800

    update页重写

--- web/src/views/UpdateLog.vue
+++ web/src/views/UpdateLog.vue
@@ -1,28 +1,4 @@
 <template>
-    <!-- <el-container>
-        <el-main>
-            <el-timeline :reverse="true">
-                <el-timeline-item
-                    v-for="(item, index) in log"
-                    :key="index"
-                    :timestamp="item.timestamp"
-                    placement="top"
-                    color="#74b9ff"
-                    size="large"
-                >
-                    <el-card class="card">
-                        <h4>{{ item.title }}</h4>
-                        <p
-                            v-for="(content, index) in item.contents"
-                            :key="index"
-                        >
-                            {{ index + 1 }} {{ content }}
-                        </p>
-                    </el-card>
-                </el-timeline-item>
-            </el-timeline>
-        </el-main>
-    </el-container> -->
     <div class="time-line">
         <div
             class="time-line-item"
@@ -30,7 +6,17 @@
             v-for="(item, index) in logs"
             :key="'log' + index"
         >
-            {{ item.timestamp }}
+            <div class="time">
+                {{ item.timestamp }}
+            </div>
+            <div class="title">
+                {{ item.title }}
+            </div>
+            <ul class="content">
+                <li v-for="(tag, index) in item.contents" :key="'c' + index">
+                    {{ tag }}
+                </li>
+            </ul>
         </div>
     </div>
 </template>
@@ -41,25 +27,50 @@ export default {
         return {
             logs: [
                 {
-                    title: '开发版本3.0.0',
-                    contents: [],
-                    timestamp: '2019-08-23'
+                    title: `V4.0.0`,
+                    contents: [`对外和管理样式分离`, `首页和时间线页再重写`],
+                    timestamp: `2020-04-12`
                 },
                 {
-                    title: '开发版本3.1.0',
-                    contents: ['登陆持久化', '新增开发日志页面'],
-                    timestamp: '2019-08-26'
+                    title: `V3.9.2`,
+                    contents: ['样式去后台化完成', '前台展示页重写完成'],
+                    timestamp: '2020-02-02'
                 },
                 {
-                    title: '开发版本3.2.0',
+                    title: `V3.9.1`,
                     contents: [
-                        '时间格式化更新（Linux和Win下格式保持一致）',
-                        '各种交互操作完全整合到vuex.actions'
+                        'Element-UI搭载基本完成',
+                        '前台展示重构减少公式化'
                     ],
-                    timestamp: '2019-08-28'
+                    timestamp: '2020-01-29'
                 },
                 {
-                    title: '开发版本3.3.0',
+                    title: 'V3.8.0',
+                    contents: ['Element-UI 重写侧栏/静态页'],
+                    timestamp: '2019-12-01'
+                },
+                {
+                    title: 'V3.7.0',
+                    contents: ['图片上传和返回', '标签排序', '后端分页器'],
+                    timestamp: '2019-11-07'
+                },
+                {
+                    title: 'V3.6.0',
+                    contents: ['Markdown编辑器样式更新，新增tool-bar'],
+                    timestamp: '2019-10-17'
+                },
+                {
+                    title: 'V3.5.0',
+                    contents: ['首页UI更改', '轮播图实现'],
+                    timestamp: '2019-10-12'
+                },
+                {
+                    title: 'V3.4.0',
+                    contents: ['评论区上线', '目体积优化'],
+                    timestamp: '2019-09-30'
+                },
+                {
+                    title: 'V3.3.0',
                     contents: [
                         '增加搜索系统',
                         '搜索相关结果高亮',
@@ -68,42 +79,22 @@ export default {
                     timestamp: '2019-09-08'
                 },
                 {
-                    title: '开发版本3.4.0',
-                    contents: ['评论区上线', '目体积优化'],
-                    timestamp: '2019-09-30'
-                },
-                {
-                    title: '开发版本3.5.0',
-                    contents: ['首页UI更改', '轮播图实现'],
-                    timestamp: '2019-10-12'
-                },
-                {
-                    title: '开发版本3.6.0',
-                    contents: ['Markdown编辑器样式更新，新增tool-bar'],
-                    timestamp: '2019-10-17'
-                },
-                {
-                    title: '开发版本3.7.0',
-                    contents: ['图片上传和返回', '标签排序', '后端分页器'],
-                    timestamp: '2019-11-07'
-                },
-                {
-                    title: '开发版本3.9.0',
-                    contents: ['Element-UI 重写侧栏/静态页'],
-                    timestamp: '2019-12-01'
-                },
-                {
-                    title: `开发版本3.9.1`,
+                    title: 'V3.2.0',
                     contents: [
-                        'Element-UI搭载基本完成',
-                        '前台展示重构减少公式化'
+                        '时间格式化更新（Linux和Win下格式保持一致）',
+                        '各种交互操作完全整合到vuex.actions'
                     ],
-                    timestamp: '2020-01-29'
+                    timestamp: '2019-08-28'
                 },
                 {
-                    title: `开发版本3.9.2`,
-                    contents: ['样式去后台化完成', '前台展示页重写完成'],
-                    timestamp: '2020-02-02'
+                    title: 'V3.1.0',
+                    contents: ['登陆持久化', '新增开发日志页面'],
+                    timestamp: '2019-08-26'
+                },
+                {
+                    title: 'V3.0.0',
+                    contents: [`部署上线`],
+                    timestamp: '2019-08-23'
                 }
             ]
         };
@@ -117,41 +108,61 @@ export default {
     justify-content: center;
     align-items: center;
     &-item {
-        min-height: 150px;
+        min-height: 100px;
         width: 400px;
         position: relative;
         background-color: white;
         border-radius: 15px;
         margin: 20px 0;
-    }
-
-    &-item:before,
-    &-item:after {
-        position: absolute;
-        top: 130px;
-        content: '';
-        width: 200px;
-        height: 10px;
-        box-shadow: 0 12px 15px rgba(0, 0, 0, 0.8);
-        z-index: -1;
-    }
-    &-item:before {
-        left: 5px;
-        -webkit-transform: rotate(-5deg);
-        -moz-transform: rotate(-5deg);
-        transform: rotate(-5deg);
-    }
-    &-item:after {
-        right: 5px;
-        -webkit-transform: rotate(5deg);
-        -moz-transform: rotate(5deg);
-        transform: rotate(5deg);
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        padding: 10px;
+        .time {
+            font-size: 0.9rem;
+            font-weight: 300;
+        }
+        .title {
+            font-weight: 400;
+            font-size: 1.1rem;
+        }
+        .content {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            font-weight: 300;
+        }
     }
     .left {
-        margin-left: -350px;
+        margin-left: -400px;
+    }
+    .left::after {
+        position: absolute;
+        top: 50%;
+        right: -20px;
+        margin-top: -20px;
+        content: '';
+        display: block;
+        width: 40px;
+        height: 40px;
+        border-radius: 10000px;
+        background-color: white;
     }
     .right {
-        margin-left: 350px;
+        margin-left: 400px;
+    }
+    .right::after {
+        position: absolute;
+        top: 50%;
+        left: 0;
+        transform: translate(-50%, -50%);
+        content: '';
+        display: block;
+        width: 40px;
+        height: 40px;
+        border-radius: 10000px;
+        background-color: white;
     }
 }
 </style>
